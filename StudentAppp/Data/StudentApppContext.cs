b60c150cda053dd4532615e3fb5343d799fa390d
USER <USER@DESKTOP-8UQ58DI>
2022-08-14 11:27:53 +0100

    Add project files.

---
+++ StudentAppp/Data/StudentApppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentAppp.Models;
+
+namespace StudentAppp.Data
+{
+    public class StudentApppContext : DbContext
+    {
+        public StudentApppContext (DbContextOptions<StudentApppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentAppp.Models.Student> Student { get; set; } = default!;
+    }
+}
